ff1b845fa0de202ffc7963ed4fa4d83d40f44e47
zhengsihan <zsh.dev@outlook.com>
2020-03-21 23:42:13 +0800

    20200321

---
+++ basic_practice/book1/ex10.py
@@ -0,0 +1,30 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+words = """
+这是固定格式？
+第一行
+第二行
+"""
+print(words)
+
+print('\'')
+print('\"')
+print('\a')
+print('a\bc')
+print('\f')
+print('hello\rthis is second line\rkk')
+print('bb is bb\vbig boss')
